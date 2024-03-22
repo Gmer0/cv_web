924782f6f09afe66aee3943404a9897256814be1
Mateusz Gmerek <tojagmerek@gmail.com>
2024-03-22 21:58:08 +0100

    Data handling fix

--- cv-editor/cv-editor.js
+++ cv-editor/cv-editor.js
@@ -1,19 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
   loadData();
+  checkVisibility()
 });
 
 function saveData() {
-  ['name', 'surname', 'email', 'phone', 'aboutMe', 'company0', 'position0'].forEach(function(field) {
+  ['name', 'surname', 'email', 'phone', 'address', 'aboutMe', 'company0', 'company1', 'company2', 'company3', 'position0', 'position1', 'position2', 'position3', 'from0', 'from1', 'from2', 'from3', 'to0', 'to1', 'to2', 'to3'].forEach(function(field) {
       var value = document.getElementById(field).value;
       localStorage.setItem(field, value);
   });
 }
 
 function loadData() {
-  ['name', 'surname', 'email', 'phone', 'aboutMe', 'company0', 'position0'].forEach(function(field) {
+  ['name', 'surname', 'email', 'phone', 'address', 'aboutMe', 'company0', 'company1', 'company2', 'company3', 'position0', 'position1', 'position2', 'position3', 'from0', 'from1', 'from2', 'from3', 'to0', 'to1', 'to2', 'to3'].forEach(function(field) {
       var value = localStorage.getItem(field);
       document.getElementById(field).value = value || '';
-      document.getElementById('preview' + field.charAt(0).toUpperCase() + field.slice(1)).textContent = value || '';
+      var previewElement = document.getElementById('preview' + field.charAt(0).toUpperCase() + field.slice(1));
+    if (previewElement) {
+      previewElement.textContent = value || '';
+    }
   });
 }
 
@@ -22,7 +26,31 @@ function showCategory(categoryName) {
   for (var i = 0; i < categories.length; i++) {
     categories[i].style.display = "none";
   }
-  document.getElementById(categoryName).style.display = "block";
+  document.getElementById(categoryName).style.display = "flex";
+}
+
+function checkVisibility() {
+  ['email', 'phone', 'address', 'aboutMe', 'company0', 'company1', 'company2', 'company3', 'position0', 'position1', 'position2', 'position3', 'from0', 'from1', 'from2', 'from3', 'to0', 'to1', 'to2', 'to3'].forEach(function(field) {
+    var value = document.getElementById(field).value.trim(); 
+    console.log(value);
+    var listItem = document.getElementById('preview' + field.charAt(0).toUpperCase() + field.slice(1) + 'ListItem');
+    console.log(listItem);
+    console.log(value.length);
+
+    if (listItem) {
+      if (value.length > 0) {
+        listItem.style.display = 'flex';
+      } else {
+        listItem.style.display = 'none';
+      }
+    }    
+  }); 
 }
 
 showCategory('main-info');
+
+function generatePDF() {
+  loadData(); // Load data before generating PDF
+  const element = document.getElementById('preview');
+  html2pdf().from(element).set({ image: { type: 'jpeg', quality: 1.0 } }).save('CV.pdf');
+}
